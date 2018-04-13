095fa402ad2cf997f6d6640e4e3c5ecfb73283ef
Usama-Aslam <uaslam345@gmail.com>
2018-04-13 18:03:50 +0500

    i make local storage form

---
+++ app.js
@@ -0,0 +1,17 @@
+var nme=document.getElementById("name");
+var email=document.getElementById("email");
+var pass=document.getElementById("password");
+function submit(){
+    checkValidation();
+    localStorage.setItem("Name",nme.value);
+    localStorage.setItem("Email",email.value);
+    localStorage.setItem("Password",pass.value);
+    return false;
+}
+
+function checkValidation(){
+    if(nme.value =="" || nme.value ==" ")
+    {
+     alert("please Enter Your Name");   
+    }
+}
